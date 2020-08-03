1f15efe04f1f4e71c236aa6731bceaa7a7408a22
Saul Figueiredo <saulfm08@gmail.com>
2020-08-03 18:19:29 -0300

    Fixes

--- product/models.py
+++ product/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib import admin
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+
+    url = models.TextField(null=True, blank=True)
+    image = models.ImageField(upload_to="products")
+    description = models.TextField(null=True, blank=True)
+
+
+    def __str__(self):
+        return self.name
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display  = ["name", "url", "image", "description"]
+    search_fields = ["name", "url", "image", "description"]
--- store/settings.py
+++ store/settings.py
@@ -127,3 +127,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = 'static'
+MEDIA_ROOT = 'static/media'
